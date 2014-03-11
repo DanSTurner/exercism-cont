55f0151c28a341ff718ff7cc98abbd37b8bcbf5e
Dan S Turner <turner@nowwithmoredan.com>
2014-03-11 14:35:15 -0700

    pythagorean-triplets refactor

--- pythagorean-triplet/triplet.rb
+++ pythagorean-triplet/triplet.rb
@@ -1,48 +1,31 @@
 class Triplet
+  attr_reader :a, :b, :c
+
   def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+    @a, @b, @c = a, b, c
   end
 
-  def sum(a = @a, b = @b, c = @c)
+  def sum
     a + b + c
   end
 
-  def product(a = @a, b = @b, c = @c)
+  def product
     a * b * c
   end
 
-  def pythagorean?(a = @a, b = @b, c = @c)
+  def pythagorean?
     a**2 + b**2 == c**2
   end
 
-  def self.where(options = {})
-    min = options[:min_factor] || 1
-    max = options[:max_factor]
-    matrix = []
-    result = []
-    (min..max).each do |c|
-      (min..c-1).each do |b|
-        (b+1..c-1).each do |a|
-          matrix << [a, b, c]
-        end
+  class << self
+    def where(min_factor: 1, max_factor: nil, sum: nil)
+      matrix = (min_factor..max_factor).to_a.combination(3).to_a
+      matrix.select! { |a, b, c| a + b + c == sum } if sum
+
+      matrix.each_with_object([]) do |(a, b, c), result|
+        triplet = new(a, b, c)
+        result << triplet if triplet.pythagorean?
       end
     end
-    if options[:sum]
-      matrix.select! do |trip|
-        test_triplet = Triplet.new(trip[0], trip[1], trip[2])
-        test_triplet.sum == options[:sum]
-      end
-    end
-    matrix.select! do |trip|
-      triplet = Triplet.new(trip[0], trip[1], trip[2])
-      triplet.pythagorean?
-    end
-    matrix.each do |trip|
-      triplet = Triplet.new(trip[0], trip[1], trip[2])
-      result << triplet
-    end
-    return result
   end
 end
